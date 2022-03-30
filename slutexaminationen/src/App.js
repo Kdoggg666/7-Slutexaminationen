c6aa04fb92610fd254a548085c77dd2e355cbbde
Kenan <kenanwright@gmail.com>
2022-03-30 20:11:05 +0200

    Add api call and variables

--- slutexaminationen/src/App.js
+++ slutexaminationen/src/App.js
@@ -1,32 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Landing from './Landing.js';
-import Menu from './Menu.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { getAllMenu, createMenu } from './services/AirBeanApi.js';
-import Axios from 'axios';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Landing from "./Landing.js";
+import Menu from "./Menu.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  let [menuItem1, setMenuItem1] = useState("");
+
   const getMenu = () => {
-    Axios.get('http://localhost:5000/api/beans').then(
-      (response) => {
-        console.log(response);
-      }
-    );
+    Axios.get("http://localhost:5000/api/beans").then((response) => {
+      console.log(response.data.menu);
+      menuItem1 = response.data.menu[0].title;
+      console.log(menuItem1);
+      
+    });
   };
-
-
   return (
     <Router>
-      <div className="App" onLoad={getMenu} >
+      <div className="App" onLoad={getMenu}>
         <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/menu' element={<Menu />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
-
       </div>
     </Router>
   );
--- slutexaminationen/src/Menu.js
+++ slutexaminationen/src/Menu.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import headImage from './/assets/graphics/graphics-header.svg';
-
+import React from "react";
+import headImage from ".//assets/graphics/graphics-header.svg";
+import Axios from "axios";
+import { useState } from "react";
 
 function Menu() {
-
-    return (
-        <div>
-        <div className="row menu">
-            <div className="col head-section-menu" style={{
-                backgroundImage: `url(${headImage})`, backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'
-            }} >
-                </div>            
-
-
+  
+  return (
+    <div>
+      <div className="row menu" >
+        <div
+          className="col head-section-menu"
+          style={{
+            backgroundImage: `url(${headImage})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+      </div>
+      <div className="row menu-row">
+        <div className="col menu-items-section">
+          <h1>Meny</h1>
         </div>
-        <div className="row menu-row">
-            <div className="col menu-items-section">
-                <h1>Meny</h1>
-                
-            </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
